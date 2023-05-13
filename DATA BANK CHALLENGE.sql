db74a9b8e0947e879745897410771fe2812458ab
Abhishek Sadawarte <109215374+j0kr97@users.noreply.github.com>
2023-05-13 14:41:22 +0530

    Update DATA BANK CHALLENGE.sql

--- DATA BANK CHALLENGE.sql
+++ DATA BANK CHALLENGE.sql
@@ -1,5 +1,4 @@
 We will need DataBase to address our problems so first we will create DATABASE 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+
+CREATE DATABASE data_bank;
+
