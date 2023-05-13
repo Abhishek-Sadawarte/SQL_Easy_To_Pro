9a1056800fc3b6c91ea9374cf24227797a4d1a9f
Abhishek Sadawarte <109215374+j0kr97@users.noreply.github.com>
2023-05-13 14:39:57 +0530

    Update and rename DATA BANK CHALLENGE to DATA BANK CHALLENGE.sql

---
+++ DATA BANK CHALLENGE.sql
@@ -0,0 +1,5 @@
+We will need DataBase to address our problems so first we will create DATABASE 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
