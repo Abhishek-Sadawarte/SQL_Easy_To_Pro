a1970aa052986b7caee71aa798e8a06aa446b15a
Abhishek Sadawarte <109215374+Abhishek-Sadawarte@users.noreply.github.com>
2023-05-17 23:54:09 +0530

    Update DATA BANK CHALLENGE.sql

--- DATA BANK CHALLENGE.sql
+++ DATA BANK CHALLENGE.sql
@@ -1,6 +1,7 @@
 We will need DataBase to address our problems so first we will create DATABASE 
 
 CREATE DATABASE data_bank; (Create syntax will make new data base for us )
+
 CREATE TABLE regions (
   region_id INTEGER,
   region_name VARCHAR(9)
