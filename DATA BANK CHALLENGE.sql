0bc3f31449b8cf0f1de9a9ebe7302ebead0fc191
Abhishek Sadawarte <109215374+Abhishek-Sadawarte@users.noreply.github.com>
2023-05-17 23:53:43 +0530

    Update DATA BANK CHALLENGE.sql

--- DATA BANK CHALLENGE.sql
+++ DATA BANK CHALLENGE.sql
@@ -1,4 +1,10 @@
 We will need DataBase to address our problems so first we will create DATABASE 
 
-CREATE DATABASE data_bank;
+CREATE DATABASE data_bank; (Create syntax will make new data base for us )
+CREATE TABLE regions (
+  region_id INTEGER,
+  region_name VARCHAR(9)
+);    (This will create table into our data base 'data_bank' with attribute like region_id region name )
+      (Integer denote the input value must be in integer data type, while VARCHAR data type is used to store variable-length character strings)
+
 
